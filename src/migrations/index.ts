9d97b446a5e8afb51cfe9c1de7d7c325f5279467
Matija <admin@mzwdesign.com>
2024-12-10 21:48:02 +0100

    Added migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241210_204724 from './20241210_204724';
+
+export const migrations = [
+  {
+    up: migration_20241210_204724.up,
+    down: migration_20241210_204724.down,
+    name: '20241210_204724'
+  },
+];
